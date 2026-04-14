b088455ce1f10bfc8d41ba63333f45b4f8ef9d11
Manya Luthra <luthramanya77@gmail.com>
2026-04-15 01:03:21 +0530

    Update DHT.py

--- Raspberry Pi with temperature sensor/DHT.py
+++ Raspberry Pi with temperature sensor/DHT.py
@@ -3,7 +3,7 @@ import Adafruit_DHT
 sensor = Adafruit_DHT.DHT11
 pin = 4
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)   
 
 if temperature is not None:
     print("Temp = {}°C".format(temperature))
